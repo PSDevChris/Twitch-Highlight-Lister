92a4e8b6888cf819e2a25f5d6161a4d5a2e8b457
PSDevChris <christopher.schmid.1993@gmail.com>
2025-02-20 16:58:38 +0100

    Updated the script to pull 100 highlights at once Removed Keyremoval, it is not needed here Changed Filemode to w to overwrite the file Formatting

--- twitch_highlight_lister.py
+++ twitch_highlight_lister.py
@@ -7,33 +7,34 @@ TWITCH_CLIENT_ID = ""
 TWITCH_TOKEN = ""
 BROADCASTER_ID = ""
 
-twitchapiurl = f"https://api.twitch.tv/helix/videos?user_id={BROADCASTER_ID}&type=highlight"
+twitchapiurl = f"https://api.twitch.tv/helix/videos?user_id={BROADCASTER_ID}&type=highlight&first=100"
 
 
 async def list_twitch_highlights():
-    async with aiohttp.ClientSession(headers={"Authorization": f"Bearer {TWITCH_TOKEN}", "Client-Id": f"{TWITCH_CLIENT_ID}"}) as session, session.get(f"{twitchapiurl}") as r:
-        # My ID is entered, change it to yours, 20 Clips are returned at max, so we have to go through pages
+    async with (
+        aiohttp.ClientSession(
+            headers={
+                "Authorization": f"Bearer {TWITCH_TOKEN}",
+                "Client-Id": f"{TWITCH_CLIENT_ID}",
+            }
+        ) as session,
+        session.get(f"{twitchapiurl}") as r,
+    ):
         clipstring = ""
         if r.status == 200:
             Clips = await r.json()
-            KeysToRemove = []
-            for key in Clips["data"][0]:  # Cleaning up the JSON to reduce memory
-                if key not in ["creator_name", "url"]:
-                    KeysToRemove.append(key)
-            for index in range(len(Clips["data"])):
-                for keyvalue in KeysToRemove:
-                    Clips["data"][index].pop(keyvalue, None)
             Pagination = Clips["pagination"]["cursor"] if Clips["pagination"] else ""
             while Pagination != "":
                 async with session.get(f"{twitchapiurl}&after={Pagination}") as r:
                     if r.status == 200:
                         NextPage = await r.json()
-                        for index in range(len(NextPage["data"])):
-                            for keyvalue in KeysToRemove:
-                                NextPage["data"][index].pop(keyvalue, None)
                         # Append new list to old one
                         Clips["data"] = Clips["data"] + NextPage["data"]
-                        Pagination = NextPage["pagination"]["cursor"] if NextPage["pagination"] else ""
+                        Pagination = (
+                            NextPage["pagination"]["cursor"]
+                            if NextPage["pagination"]
+                            else ""
+                        )
             print(f"Loaded the Twitch Clips, I found {len(Clips['data'])} Clips.")
         else:
             print("ERROR: Twitch Clips could not be loaded!")
@@ -43,7 +44,7 @@ async def list_twitch_highlights():
 
 
 async def write_cliplist():
-    with open("highlights.txt", "+a") as cliplist:
+    with open("highlights.txt", "w") as cliplist:
         cliplist.write(await list_twitch_highlights())
 
 
